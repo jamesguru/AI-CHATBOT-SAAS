cbadfeb341b6e132cfdb3f9ed8b72872afc2f80a
unknown <jameskagunga2022@gmail.com>
2024-06-12 13:45:36 +0300

    added changes

---
+++ src/components/forms/sign-up/type-selection-form.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {FieldValues, UseFormRegister} from 'react-hook-form'
+type Props = {
+    register:UseFormRegister<FieldValues>
+    userType: 'owner' | 'student'
+    setUserType: React.Dispatch<React.SetStateAction<'owner' | 'student'>>
+}
+
+const TypeSelectionForm = (props: Props) => {
+  return (
+    <div>TypeSelectionForm</div>
+  )
+}
+
+export default TypeSelectionForm
